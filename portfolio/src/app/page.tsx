48d9c9135860eadb7a88547e5a7396ec96f718f5
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-04-14 17:38:03 +0530

    workpage done

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -8,20 +8,68 @@ export default function Home() {
       <WorkPage
         images={[
           {
-            src: "https://static.vecteezy.com/system/resources/thumbnails/008/612/703/small/html-code-on-computer-monitor-software-web-developer-programming-code-photo.jpg",
+            src: "https://m.media-amazon.com/images/I/61R-GKU5sBL.jpg",
             scale: 4,
           },
           {
             src: "https://t3.ftcdn.net/jpg/02/14/87/96/360_F_214879686_R3HFJlk6WLr1kcdvy6Q9rtNASKN0BZBS.jpg",
             scale: 5,
+            position: {
+              top: "65vh",
+              left: "60vw",
+              width: "35vw",
+              height: "30vh",
+            },
           },
           {
             src: "https://static.vecteezy.com/system/resources/thumbnails/008/612/703/small/html-code-on-computer-monitor-software-web-developer-programming-code-photo.jpg",
             scale: 6,
+            position: {
+              top: "-10vh",
+              left: "-25vw",
+              width: "20vw",
+              height: "45vh",
+            },
           },
           {
             src: "https://t3.ftcdn.net/jpg/02/14/87/96/360_F_214879686_R3HFJlk6WLr1kcdvy6Q9rtNASKN0BZBS.jpg",
             scale: 7,
+            position: {
+              top: "0vh",
+              left: "27.5vw",
+              width: "25vw",
+              height: "25vh",
+            },
+          },
+          {
+            src: "https://static.vecteezy.com/system/resources/thumbnails/008/612/703/small/html-code-on-computer-monitor-software-web-developer-programming-code-photo.jpg",
+            scale: 5,
+            position: {
+              top: "27.5vh",
+              left: "5vw",
+              width: "20vw",
+              height: "25vh",
+            },
+          },
+          {
+            src: "https://t3.ftcdn.net/jpg/02/14/87/96/360_F_214879686_R3HFJlk6WLr1kcdvy6Q9rtNASKN0BZBS.jpg",
+            scale: 4,
+            position: {
+              top: "27.5vh",
+              left: "-22.5vw",
+              width: "30vw",
+              height: "25vh",
+            },
+          },
+          {
+            src: "https://static.vecteezy.com/system/resources/thumbnails/008/612/703/small/html-code-on-computer-monitor-software-web-developer-programming-code-photo.jpg",
+            scale: 3,
+            position: {
+              top: "22.5vh",
+              left: "25vw",
+              width: "15vw",
+              height: "15vh",
+            },
           },
         ]}
       />
---
+++ portfolio/src/components/WorkPage.tsx
@@ -0,0 +1,91 @@
+import React, { useRef, useEffect } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+interface ImageProps {
+  src: string;
+  scale: number;
+  position?: {
+    top?: string;
+    left?: string;
+    width?: string;
+    height?: string;
+  };
+}
+
+interface WorkPageProps {
+  images: ImageProps[];
+}
+
+const WorkPage: React.FC<WorkPageProps> = ({ images = [] }) => {
+  const containerRef = useRef(null);
+  const imageContainerRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  useEffect(() => {
+    imageContainerRefs.current = Array(images.length).fill(null);
+  }, [images.length]);
+
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 0.5,
+        pin: false,
+        markers: false,
+      },
+    });
+
+    imageContainerRefs.current.forEach((ref, index) => {
+      if (ref) {
+        tl.fromTo(
+          ref,
+          { scale: 1 },
+          {
+            scale: images[index].scale * 1,
+            ease: "none",
+          },
+          0
+        );
+      }
+    });
+  }, [images]);
+
+  return (
+    <div ref={containerRef} className="h-[300vh] relative my-[100vh]">
+      <div className="sticky top-0 h-[100vh] bg-orange-400 overflow-hidden">
+        {images.map((image, index) => (
+          <div
+            ref={(el) => {
+              imageContainerRefs.current[index] = el;
+            }}
+            key={index}
+            className="w-full h-full absolute top-0 flex items-center justify-center"
+          >
+            <div
+              className="absolute origin-center"
+              style={{
+                width: image.position?.width || "25vw",
+                height: image.position?.height || "25vh",
+                top: image.position?.top || "auto",
+                left: image.position?.left || "auto",
+              }}
+            >
+              <img
+                src={image.src}
+                alt=""
+                className="object-cover w-full h-full"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default WorkPage;
